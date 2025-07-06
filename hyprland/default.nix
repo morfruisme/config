34c952ebd98e29a4c30d2f2d660faf49e00de139
fruit <nico.travert@gmail.com>
2025-07-06 16:17:42 +0200

    🪲

--- hyprland/default.nix
+++ hyprland/default.nix
@@ -1,11 +1,6 @@
 { ... }:
 
 {
-  wayland.windowManager.hyprland = {
-    enable = true;
-    systemd.enable = true;
-  };
-
   home.file."./config/hypr/hyprland.conf".source = ./hyprland.conf;
   
   services.hyprsunset.enable = true;
