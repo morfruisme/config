97a7f42566e89433f34539bfb9181be5403c3c84
fruit <nico.travert@gmail.com>
2025-08-13 17:12:39 +0200

    🐣

---
+++ madeleine/hyprland.nix
@@ -0,0 +1,10 @@
+{ ... }:
+
+{
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "HDMI-A-1, 1920x1080, 0x0, 1, bitdepth, 10, cm, hdr"
+      "HDMI-A-2, 1920x1080, -1980x0, 1, bitdepth, 10, cm, hdr"
+    ];
+  };
+}
