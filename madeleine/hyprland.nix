a5cf50a7027e20b3f72c97eb54af60c95cd52837
fruit <nico.travert@gmail.com>
2025-08-14 15:01:46 +0200

    alignage des ecrans !

--- madeleine/hyprland.nix
+++ madeleine/hyprland.nix
@@ -3,9 +3,8 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "HDMI-A-1, 1920x1080, 0x0, 0.833333"
-      "HDMI-A-2, 1920x1080, -1980x200, 1"
-      ", preferred, auto, 1"
+      "desc:Philips Consumer Electronics Company PHL 271E1 0x0000089F, 1920x1080, 0x0, 0.8"
+      "desc:Acer Technologies Acer KG221Q 0x83108499, 1920x1080, -1920x250, 1"
     ];
   };
 }
